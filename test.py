90c03224aee930db571cdae492b2a43c230cca1f
shayanfh <shayan.famous79@gmail.com>
2026-04-28 09:35:04 +0330

    remove extra comments

--- test.py
+++
@@ -1,8 +0,0 @@
-Book time to meet with me
-
-________________________________
-From: Guest2
-Sent: Tuesday, January 6, 2026 18:35
-To: Eric F <EricF@reach360.solutions>; Fy R <FyR@reach360.solutions>
-Cc: Archive <archive@reach360.solutions>
-Subject: Demo of email a
