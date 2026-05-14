db4042f18f68fa73f9dcd305cab61bd57c37c7f8
shayanfh <shayan.famous79@gmail.com>
2026-05-14 09:46:08 +0330

    create db

--- wsgi.py
+++ wsgi.py
@@ -3,3 +3,7 @@ from app import create_app
 # Create app with all blueprints already registered
 # (backfill_scheduler blueprint is registered in application.py)
 app = create_app()
+
+from app.models import init_db
+
+init_db(app)
